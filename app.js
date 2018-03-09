77440d63771cd99b17f462c0fdb78ebb7856c7aa
Francisco Rois <francisco.rois_siso@daimler.com>
2018-03-09 15:20:38 -0800

    Deleted extra unneeded code

--- app.js
+++ app.js
@@ -100,18 +100,3 @@ app.post('/proxytocors/', function(req,res){
 		res.send("POST received");
 	});
 });
-
-var request = require("request");
-
-var options = { method: 'POST',
- url: 'https://notification-test.germanycentral.cloudapp.microsoftazure.de/api/Notification/Create/Car',
- headers:
-  { 'Postman-Token': '7ce75ce7-4e83-f248-f6f8-6ec0f3c8c29c',
-    'Cache-Control': 'no-cache' },
- body: '{\r\n  "Category": "Information",\r\n  "FinOrVin": "WDD1770031Z000032",\r\n  "NotificationUID": '+generated_guid.value+',\r\n  "SourceServiceName": "Postman",\r\n  "Priority": "Default",\r\n  "Type": "UserNotification",\r\n  "TimeDefinition":{"ActivationStartTime":"2006-01-02T15:04:05Z","ActivationEndTime":"2019-01-02T15:04:05Z"},\r\n\r\n  "Notification": {\r\n  \r\n    "ActionTemplate": [{\r\n      "Arguments": [\r\n        {\r\n          "Arguments": "{\\"DestinationID\\":0,\\"Destination\\":{\\"Title\\":\\"Hausbrauerei Moenchwasen\\",\\"City\\":\\"Simmozheim\\",\\"Street\\":\\"Im Moenchgraben\\",\\"HouseNumber\\":\\"30\\",\\"CrossRoad\\":null,\\"PostalCode\\":\\"75397\\",\\"Country\\":\\"Deutschland\\",\\"CountryIsoCode\\":null,\\"State\\":\\"Baden Würtemberg\\",\\"Lon\\":8.806573,\\"Lat\\":48.745373,\\"AirDistance\\":0.0,\\n\\"DetourTimeInMinutes\\": 0,\\n\\"Type\\": \\"Tourist\\"\\n  },\\n\\"Name\\": null,\\n\\"Destinations\\": null\\n}",\r\n          "LanguageCode": "default"\r\n        }\r\n      ],\r\n      "Group": "Navigation",\r\n      "ID": 196609,\r\n      "Name": "AddAsNavigationTarget",\r\n      "Text": [\r\n        {\r\n          "LanguageCode": "default",\r\n          "Text": "Navigate To"\r\n        }\r\n      ],\r\n      "TrustLevelForExecution": 0\r\n    }],\r\n  \r\n  "MessageLargeIcon": {       \r\n\t"IconResourceUID": "testIconAMGGTR"  \r\n\t},  \r\n    \r\n    "Message": [\r\n      {\r\n        "LanguageCode": "default",\r\n        "PrimaryText": "PN + Navi Action (Add As Navigation Target)",\r\n        "ReadOut": "Vorlesen Deutsch",\r\n        "SecondaryText": ""\r\n      }\r\n    ]\r\n  }\r\n}\r\n' };
-
-request(options, function (error, response, body) {
- if (error) throw new Error(error);
-
- console.log(body);
-});
